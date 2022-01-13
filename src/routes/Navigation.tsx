c5d72be7f8f6e5b1ca98bb05ed3968c3d62d9f56
cesar londoño <slac_@hotmail.com>
2022-01-13 00:04:42 -0300

    first commit

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter } from "react-router-dom"
+import {
+    Routes,
+    Route,
+    NavLink
+  } from "react-router-dom";
+
+  import logo from '../logo.svg';
+
+export const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={ logo } alt="React Logo"/>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/users">Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                        <Route path="about" element={<h1>HolA</h1>} />
+                        <Route path="users" element={<h1>HolAS</h1>} />
+                        <Route path="/" element={<h1>HolASS</h1>} />
+                    
+                </Routes>
+                
+            </div>
+        </BrowserRouter>
+    )
+}
